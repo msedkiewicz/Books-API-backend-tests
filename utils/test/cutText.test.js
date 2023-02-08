1ed82f28b324e663a88bcdc5ba6cae8b38306762
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-08 18:44:56 +0100

    Add test for content length

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -1,13 +1,16 @@
-const cutText = require('../cutText.js');
-const expect = require('chai').expect;
+const cutText = require("../cutText.js");
+const expect = require("chai").expect;
 
-describe('cutText', () => {
-    it('should return an error if "content" arg is not as string', () => {
-        if (typeof content !== 'string') return 'Error';
-        expect(cutText(undefined, 20)).to.equal('Error');
-        expect(cutText(12, 20)).to.equal('Error');
-        expect(cutText({}, 20)).to.equal('Error');
-        expect(cutText([], 20)).to.equal('Error');
-        expect(cutText(function () {}, 20)).to.equal('Error');
-    });
+describe("cutText", () => {
+  it('should return an error if "content" arg is not as string', () => {
+    if (typeof content !== "string") return "Error";
+    expect(cutText(undefined, 20)).to.equal("Error");
+    expect(cutText(12, 20)).to.equal("Error");
+    expect(cutText({}, 20)).to.equal("Error");
+    expect(cutText([], 20)).to.equal("Error");
+    expect(cutText(function () {}, 20)).to.equal("Error");
+  });
+  it('should return an error if "content" arg length is 0', () => {
+    expect(cutText("", 20)).to.equal("Error");
+  });
 });
