ab27b58cd891241e2226c766e06dabac29f18a30
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-08 19:04:36 +0100

    Add test for more than 2 blocks; code formatting

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -1,26 +1,28 @@
-const formatFullname = require('../formatFullname');
-const expect = require('chai').expect;
+const formatFullname = require("../formatFullname");
+const expect = require("chai").expect;
 
-
-describe('formatFullname', () => {
-    it('should return an error if "fullname" arg is not as string', () => {
-        if (typeof fullname !== 'string') return 'Error';
-        expect(formatFullname(undefined)).to.equal('Error');
-        expect(formatFullname(12)).to.equal('Error');
-        expect(formatFullname({})).to.equal('Error');
-        expect(formatFullname([])).to.equal('Error');
-        expect(formatFullname(function () {})).to.equal('Error');
-    });
-    it('should return an error if "formatFullName" arg length is 0', () => {
-        expect(formatFullname()).to.equal('Error');
-    });
-    it('should return proper formatting "formatFullName" for args', () => {
-        expect(formatFullname('JOHN DOE')).to.equal('John Doe');
-        expect(formatFullname('johN dOe')).to.equal('John Doe');
-        expect(formatFullname('john doe')).to.equal('John Doe');
-    });
-    it('should return an error if one of "formatFullName" blocks length is 0', () => {
-        expect(formatFullname('John')).to.equal('Error');
-        expect(formatFullname('Doe')).to.equal('Error');
-    });
+describe("formatFullname", () => {
+  it('should return an error if "fullname" arg is not as string', () => {
+    if (typeof fullname !== "string") return "Error";
+    expect(formatFullname(undefined)).to.equal("Error");
+    expect(formatFullname(12)).to.equal("Error");
+    expect(formatFullname({})).to.equal("Error");
+    expect(formatFullname([])).to.equal("Error");
+    expect(formatFullname(function () {})).to.equal("Error");
+  });
+  it('should return an error if "formatFullName" arg length is 0', () => {
+    expect(formatFullname()).to.equal("Error");
+  });
+  it('should return proper formatting "formatFullName" for args', () => {
+    expect(formatFullname("JOHN DOE")).to.equal("John Doe");
+    expect(formatFullname("johN dOe")).to.equal("John Doe");
+    expect(formatFullname("john doe")).to.equal("John Doe");
+  });
+  it('should return an error if one of "formatFullName" blocks length is 0', () => {
+    expect(formatFullname("John")).to.equal("Error");
+    expect(formatFullname("Doe")).to.equal("Error");
+  });
+  it('should return an error if "formatFullName" have more than 2 blocks', () => {
+    expect(formatFullname("John Doe Smith")).to.equal("Error");
+  });
 });
