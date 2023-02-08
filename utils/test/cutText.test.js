0299c081d80f6f5ead1e8ba93de003d098f2c60d
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-08 18:43:29 +0100

    Add arguments testing

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -1,12 +1,13 @@
-const cutText = require("../cutText.js");
-const expect = require("chai").expect;
+const cutText = require('../cutText.js');
+const expect = require('chai').expect;
 
-describe("CutText", () => {
-  it('should return an error if "content" arg is not a string', () => {
-    expect(cutText(undefined, 20)).to.equal("Error");
-    expect(cutText(12, 20)).to.equal("Error");
-    expect(cutText({}, 20)).to.equal("Error");
-    expect(cutText([], 20)).to.equal("Error");
-    expect(cutText(function () {}, 20)).to.equal("Error");
-  });
+describe('cutText', () => {
+    it('should return an error if "content" arg is not as string', () => {
+        if (typeof content !== 'string') return 'Error';
+        expect(cutText(undefined, 20)).to.equal('Error');
+        expect(cutText(12, 20)).to.equal('Error');
+        expect(cutText({}, 20)).to.equal('Error');
+        expect(cutText([], 20)).to.equal('Error');
+        expect(cutText(function () {}, 20)).to.equal('Error');
+    });
 });
