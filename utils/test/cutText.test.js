6654d74fcd6bb6909a068a9eb1e49825ace537b3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-08 18:47:13 +0100

    Add test for maxLength (if it's a number)

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -13,4 +13,11 @@ describe("cutText", () => {
   it('should return an error if "content" arg length is 0', () => {
     expect(cutText("", 20)).to.equal("Error");
   });
+  it('should return an error if "maxLength" arg is not a number', () => {
+    expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+    expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+    expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+    expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+    expect(cutText('Lorem Ipsum', function() {})).to.equal('Error');
+  });
 });
